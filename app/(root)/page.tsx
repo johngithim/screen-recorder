905c2aa75727578d54aa398f478101a4b01a75da
John <johnmuller0910@gmail.com>
2025-05-25 21:39:57 +0300

    upcoming cards

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+
+const Page = () => {
+  return (
+    <main className={"wrapper page"}>
+      <Header title={"All Videos"} subHeader={"Public library"} />
+
+      <h1>Hello</h1>
+    </main>
+  );
+};
+
+export default Page;
